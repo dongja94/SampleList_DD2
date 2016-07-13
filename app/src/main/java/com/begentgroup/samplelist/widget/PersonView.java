51031c6978661ab4729355cbad12bce64829a092
Yon Seungik <dongja94@gmail.com>
2016-07-13 11:00:41 +0900

    interface

--- app/src/main/java/com/begentgroup/samplelist/widget/PersonView.java
+++ app/src/main/java/com/begentgroup/samplelist/widget/PersonView.java
@@ -2,6 +2,7 @@ package com.begentgroup.samplelist.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +26,15 @@ public class PersonView extends FrameLayout {
     ImageView photoView;
     TextView nameView, ageView;
 
+    public interface OnImageClickListener {
+        public void onImageClick(PersonView view, Person person);
+    }
+
+    OnImageClickListener mListener;
+    public void setOnImageClickListener(OnImageClickListener listener) {
+        mListener = listener;
+    }
+
     private void init() {
 //        LayoutInflater inflater = LayoutInflater.from(getContext());
 //        inflater.inflate(R.layout.view_person, this);
@@ -32,6 +42,14 @@ public class PersonView extends FrameLayout {
         photoView = (ImageView)findViewById(R.id.image_photo);
         nameView = (TextView)findViewById(R.id.text_name);
         ageView = (TextView)findViewById(R.id.text_age);
+        photoView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onImageClick(PersonView.this, person);
+                }
+            }
+        });
     }
 
     Person person;
